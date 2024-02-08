9f37fcab529eefc8f96de269175104ecbb693458
mhsaim <151901425+mhsaim@users.noreply.github.com>
2024-02-09 02:47:55 +0600

    Beecrowd Problem 1005
    Try Your Best

---
+++ B1005.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float A,B, MEDIA;
+    A= A*3.5;
+    B= B*7.5;
+    scanf("%f %f", &A, &B);
+     A= A*3.5;
+    B= B*7.5;
+    MEDIA = (A + B)/(3.5+7.5);
+    printf("MEDIA = %.5f\n",MEDIA);
+    return 0;
+}
