05bb4400ebdc601dc64e25f12732def6fbeca6fe
mhsaim <151901425+mhsaim@users.noreply.github.com>
2024-02-09 02:26:51 +0600

    Test
    First C Programming

---
+++ 1st.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=1;i<20;i++)
+    {
+        if(i%3==0)
+          {
+              printf("%d\n",i);
+             continue;
+          }
+        else if(i==10)
+            break;
+    }
+    return 0;
+}
